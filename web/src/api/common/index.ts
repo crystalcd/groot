0d83f56df2a3587c8cc672c46a1430278a23455c
crystal <1627566612@qq.com>
2023-07-11 02:03:21 +0800

    11

--- web/src/api/common/index.ts
+++ web/src/api/common/index.ts
@@ -4,6 +4,47 @@ import type { IUser } from '../system/modal/userModel'
 
 // 菜单
 export function getMenus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          "id": "home",
+          "name": "首页",
+          "icon": "HomeFilled",
+          "path": "/home"
+        },
+        {
+          "id": "base",
+          "name": "基础管理",
+          "icon": "Management",
+          "children": [
+            {
+              "id": "article",
+              "name": "文章管理",
+              "path": "/base/article"
+            }
+          ]
+        },
+        {
+          "id": "system",
+          "name": "系统管理",
+          "icon": "Management",
+          "children": [
+            {
+              "id": "user",
+              "name": "用户管理",
+              "path": "/system/user"
+            },
+            {
+              "id": "role",
+              "name": "角色管理",
+              "path": "/system/role"
+            }
+          ]
+        }
+      ])
+    }, 2000);
+  });
   return get<IMenuItem[]>('/menu')
 }
 
