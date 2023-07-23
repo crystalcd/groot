f5f4d35ecc91a97a0200a12eeb27e3351f51b53b
crystal <1627566612@qq.com>
2023-07-24 01:00:28 +0800

    refactor

--- server/usecase/subfinder_usecase.go
+++ server/usecase/subfinder_usecase.go
@@ -1,1 +1,24 @@
 package usecase
+
+import (
+	"github.com/crystal/groot/domain"
+)
+
+type subfinderUseCase struct {
+	domain.DomainScan
+}
+
+func NewSubfinderUseCase() *subfinderUseCase {
+	subfinder := &subfinderUseCase{
+		domain.DomainScan{
+			Config: domain.Config{
+				Path: "123",
+			},
+		},
+	}
+	subfinder.Cmd = subfinder
+	return subfinder
+}
+
+func (s *subfinderUseCase) Run(domian string) {
+}
