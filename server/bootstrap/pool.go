7fc65ccab4d25d0289fe62c3d398e8845f8acefc
byronc <byron.chen@zoom.us>
2023-07-26 18:10:36 +0800

    refactor

--- server/bootstrap/pool.go
+++ server/bootstrap/pool.go
@@ -2,9 +2,10 @@ package bootstrap
 
 import (
 	"github.com/panjf2000/ants/v2"
+	"github.com/sirupsen/logrus"
 )
 
-func NewPool(env *Env) *ants.Pool {
-	pool, _ := ants.NewPool(env.AsyncPoolCount)
+func NewPool(env *Env, *logrus.Logger) *ants.Pool {
+	pool, err := ants.NewPool(env.AsyncPoolCount)
 	return pool
 }
